e28c3c10e5aab3ab914061a7fcf25e77dc70bc54
Austin Wolf <awolf@illumina.com>
2025-02-15 17:52:26 -0800

    build static lib

---
+++ c/test_sensor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+void sensor_init(void) 
+{
+    printf("Hello from C!\n");
+}
+
+int sensor_get_value(void) 
+{
+    return 42;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
+
+extern "C" {
+    fn sensor_init();
+    fn sensor_get_value() -> i32;
+}
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("OUT_DIR", "./out_dir");
+    env::set_var("TARGET", "x86_64-unknown-linux-gnu");
+    env::set_var("HOST", "x86_64-unknown-linux-gnu");
+    env::set_var("OPT_LEVEL", "0");
+
+    println!("Hello, world1!");
+
+    cc::Build::new()
+    .file("c/test_sensor.c")
+    .flag("-g")
+    .compile("test");
 }
